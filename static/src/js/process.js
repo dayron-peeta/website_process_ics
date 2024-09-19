6e7d251ff2da7e497a1aa2fb0f4093216805e002
DPeeta <dayronalejandrocruzpita@gmail.com>
2024-09-19 08:26:25 -0400

    comprobaciones_js 1.2 hasta representation_license muestra y oculta

--- static/src/js/process.js
+++ static/src/js/process.js
@@ -5,6 +5,7 @@ odoo.define('df_website_process_ics.process', function (require) {
     $(document).ready(function () {
         console.log('//////////////////////////////////////////////////////////////////////////////////////////////¡Hola desde process.js!');
 
+        ///////////////////////////////////////////////////////////
         // Función genérica para MOSTRAR/OCULTAR CAMPOS basados en la selección y cambiar el atributo 'required'
         function toggleField(selectId, targetId, valuesToShow, clearChildren = []) {
             var select = document.getElementById(selectId);
@@ -38,21 +39,50 @@ odoo.define('df_website_process_ics.process', function (require) {
             }
         }
 
-        // Asocia el evento `change` al select de `person_type`
+        // Asocia el evento `change` al select 
         $('#person_type').change(function () {
             toggleField('person_type', 'entity_type_group', ['legal_cuban', 'legal_foreign'], ['full_name_group', 'other_full_name_group']);
         });
 
-        // Asocia el evento `change` al select de `entity_type`
         $('#entity_type').change(function () {
             toggleField('entity_type', 'full_name_group', ['state'], ['other_full_name_group']);
         });
 
-        // Asocia el evento `change` al select de `full_name`
         $('#full_name').change(function () {
             toggleField('full_name', 'other_full_name_group', ['other']);
         });
 
+        $('#person_type').change(function () {
+            toggleField('person_type', 'representation_license_group', ['national_foreign', 'legal_foreign']);
+        });
+        
+        ////////////////////////////////////////////////////////////////////
+        // Función genérica para MOSTRAR/OCULTAR CAMPOS si se escribe en un input y cambiar el atributo 'required'
+        function toggleFieldInput(inputId, targetId) {
+            var inputField = document.getElementById(inputId);
+            var targetGroup = document.getElementById(targetId);
+            var targetInput = targetGroup.querySelector('input, select'); // Busca el input o select dentro del div
+        
+            inputField.addEventListener('input', function () {
+                if (inputField.value.trim() !== '') {  // Verifica si el campo no está vacío
+                    targetGroup.style.display = 'block';  // Muestra el campo
+                    if (targetInput) {
+                        targetInput.setAttribute('required', 'true');  // Lo hace requerido
+                    }
+                } else {
+                    targetGroup.style.display = 'none';  // Oculta el campo
+                    if (targetInput) {
+                        targetInput.removeAttribute('required');  // Quita el atributo requerido
+                        targetInput.value = ''; // Elimina el valor del campo oculto para que no se envíe
+                    }
+                }
+            });
+        }
+        
+        // Asocia el evento `change` al input `cup_account_number`        
+        toggleFieldInput('cup_account_number', 'cup_bank_branch_group');
+        toggleFieldInput('mlc_account_number', 'mlc_account_number_group');
+        toggleFieldInput('cc_account_number', 'cc_bank_branch_group');
 
 
         $(document).ready(function () {
